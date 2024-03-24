a1f4d3bffc200f826eaddb2c70a5dcc4a5589fce
Francisco J Vizcaya <franciscovl@gmail.com>
2024-03-24 01:13:54 +0100

    Modificación memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fvizcaya <fvizcaya@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fvizcaya <fvizcaya@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 18:14:35 by fvizcaya          #+#    #+#             */
-/*   Updated: 2024/03/14 20:46:02 by fvizcaya         ###   ########.fr       */
+/*   Updated: 2024/03/24 01:04:38 by fvizcaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,25 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char		*ptrdst;
 	const unsigned char	*ptrsrc;
+	size_t				i;
 
 	ptrdst = (unsigned char *) dst;
 	ptrsrc = (const unsigned char *) src;
-	if (dst == NULL || src == NULL || dst == src)
+	i = 0;
+	if (ptrdst == NULL || ptrsrc == NULL || ptrdst == ptrsrc)
 		return (dst);
-	while (len--)
-		*ptrdst++ = *ptrsrc++;
+	if (ptrdst > ptrsrc && ptrdst - ptrsrc < (long int) len)
+		while (len--)
+			*ptrdst++ = *ptrsrc++;
+	else if (ptrsrc > ptrdst && ptrsrc - ptrdst < (long int) len)
+	{
+		while (i < len)
+		{
+			*ptrdst++ = *ptrsrc++;
+			i++;
+		}
+	}
+	else
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
