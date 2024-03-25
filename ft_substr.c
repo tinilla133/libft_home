6f203688f9539ab19b75c63ecbcdcd557f6bfa46
Francisco Jose Vizcaya Lopez <fvizcaya@c2r16s4.42madrid.com>
2024-03-25 23:13:23 +0100

    Arreglo de ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fvizcaya <fvizcaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:05:06 by fvizcaya          #+#    #+#             */
-/*   Updated: 2024/03/25 21:40:25 by fvizcaya         ###   ########.fr       */
+/*   Updated: 2024/03/25 23:07:53 by fvizcaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*buff;
 
-	if (s == NULL)
+	if (s == NULL || start > ft_strlen(s))
 		return (NULL);
 	buff = (char *) malloc (len * sizeof (char));
 	if (buff == NULL)
